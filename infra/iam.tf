41a85f8d00508449a81df372009e4e714beacd20
noob-developer25 <alanbalnour153@gmail.com>
2025-06-15 15:35:21 -0400

    Added output and iam tf files

---
+++ infra/iam.tf
@@ -0,0 +1,65 @@
+# infra/iam.tf
+
+# IAM Role that the EC2 instance will assume to get AWS permissions
+resource "aws_iam_role" "ec2_role" {
+  name = "citytaster-ec2-role"
+
+  # This trust policy lets EC2 instances assume the role
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"  # Only EC2 can assume this role
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+# Policy: EC2 can pull Docker images from ECR and read CSV files from S3
+resource "aws_iam_policy" "ec2_policy" {
+  name        = "citytaster-ec2-policy"
+  description = "Allow EC2 to access ECR and S3 buckets"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      # ECR permissions: pull images
+      {
+        Effect = "Allow",
+        Action = [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage"
+        ],
+        Resource = "*"
+      },
+      # S3 permissions: list and get objects in your bucket
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:GetObject",
+          "s3:ListBucket"
+        ],
+        Resource = [
+          "arn:aws:s3:::${aws_s3_bucket.csv_data.bucket}",      # Bucket itself
+          "arn:aws:s3:::${aws_s3_bucket.csv_data.bucket}/*"    # All objects inside bucket
+        ]
+      }
+    ]
+  })
+}
+
+# Attach the above policy to the EC2 role
+resource "aws_iam_role_policy_attachment" "attach" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.ec2_policy.arn
+}
+
+# Create an Instance Profile for EC2 (this is how you assign a role to an instance)
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "citytaster-ec2-profile"
+  role = aws_iam_role.ec2_role.name
+}
---
+++ infra/outputs.tf
@@ -0,0 +1,37 @@
+# infra/outputs.tf
+
+# Output the public IP address of the EC2 instance (use to SSH or set as GitHub secret)
+output "ec2_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.main.public_ip
+}
+
+# Output the DNS name of the EC2 instance (sometimes easier than IP)
+output "ec2_public_dns" {
+  description = "Public DNS name of the EC2 instance"
+  value       = aws_instance.main.public_dns
+}
+
+# Output the name of your S3 bucket (use for data sync in CI/CD)
+output "s3_bucket_name" {
+  description = "S3 bucket for CSV/data files"
+  value       = aws_s3_bucket.csv_data.bucket
+}
+
+# Output the ECR repository URI for the backend (use for Docker image pushes)
+output "ecr_backend_url" {
+  description = "ECR repo URI for backend"
+  value       = aws_ecr_repository.backend.repository_url
+}
+
+# Output the ECR repository URI for the frontend (use for Docker image pushes)
+output "ecr_frontend_url" {
+  description = "ECR repo URI for frontend"
+  value       = aws_ecr_repository.frontend.repository_url
+}
+
+# Output the AWS Account ID (useful for ECR image tagging in CI/CD)
+output "aws_account_id" {
+  description = "AWS account ID"
+  value       = data.aws_caller_identity.current.account_id
+}
